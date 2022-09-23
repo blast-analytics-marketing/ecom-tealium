af8461bc2b5b338779513eb81277467091fe2f41
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2022-09-23 15:02:26 +0100

    Install Tealium

--- pages/_document.js
+++ pages/_document.js
@@ -43,11 +43,16 @@ class MyDocument extends Document {
           <meta name="twitter:card" content="summary_large_image" />
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
+              __html: `var utag_data = {}`,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(a,b,c,d){
+                a='https://tags.tiqcdn.com/utag/${process.env.TEALIUM_ACCOUNT}/${process.env.TEALIUM_PROFILE}/${process.env.TEALIUM_ENV}/utag.js';
+                b=document;c='script';d=b.createElement(c);d.src=a;d.type='text/java'+c;d.async=true;
+                a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
+                })();`,
             }}
           />
         </Head>
